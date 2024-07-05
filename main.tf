2a024fe6a6d410490265927b93ca5b9eadaf44c9
Cristian Beraha <cristian@cloudops.works>
2024-07-05 19:58:14 -0300

    chore: advanced

--- main.tf
+++ main.tf
@@ -3,32 +3,28 @@
 #            On GitHub: https://github.com/cloudopsworks
 #            Distributed Under Apache v2.0 License
 #
-locals {
-  # all names are prefixed with tg-
-  short_name = var.name_prefix != "" ? format("tg-%s-%s", var.name_prefix, local.system_name_short) : format("tg-%s", local.system_name_short)
-  long_name  = var.name_prefix != "" ? format("tg-%s-%s", var.name_prefix, local.system_name) : format("tg-%s", local.system_name)
-}
 
 # Target group definition
 resource "aws_lb_target_group" "this" {
-  name                               = local.short_name
-  target_type                        = var.target_type
-  port                               = var.port
-  preserve_client_ip                 = var.preserve_client_ip
-  protocol                           = var.protocol
-  protocol_version                   = var.protocol_version
-  proxy_protocol_v2                  = var.proxy_protocol_v2
-  vpc_id                             = var.vpc_id != "" ? var.vpc_id : null
-  connection_termination             = var.connection_termination
-  deregistration_delay               = var.deregistration_delay
-  slow_start                         = var.slow_start
-  ip_address_type                    = var.ip_address_type != "" ? var.ip_address_type : null
-  load_balancing_algorithm_type      = var.load_balancing_algorithm_type
-  load_balancing_anomaly_mitigation  = var.load_balancing_anomaly_mitigation
-  load_balancing_cross_zone_enabled  = var.load_balancing_cross_zone_enabled
-  lambda_multi_value_headers_enabled = var.lambda_multi_value_headers_enabled
+  for_each                           = var.target_groups
+  name                               = format("%s-%s", each.key, local.system_name_short)
+  target_type                        = try(each.value.target_type, "instance")
+  port                               = try(each.value.port, 80)
+  preserve_client_ip                 = try(each.value.preserve_client_ip, null)
+  protocol                           = try(each.value.protocol, "HTTP")
+  protocol_version                   = try(each.value.protocol_version, "HTTP1")
+  proxy_protocol_v2                  = try(each.value.proxy_protocol_v2, false)
+  vpc_id                             = try(each.value.vpc_id, null)
+  connection_termination             = try(each.value.connection_termination, false)
+  deregistration_delay               = try(each.value.deregistration_delay, 300)
+  slow_start                         = try(each.value.slow_start, 0)
+  ip_address_type                    = try(each.value.ip_address_type, "ipv4")
+  load_balancing_algorithm_type      = try(each.value.load_balancing_algorithm_type, "round_robin")
+  load_balancing_anomaly_mitigation  = try(each.value.load_balancing_anomaly_mitigation, null)
+  load_balancing_cross_zone_enabled  = try(each.value.load_balancing_cross_zone_enabled, false)
+  lambda_multi_value_headers_enabled = try(each.value.lambda_multi_value_headers_enabled, null)
   dynamic "stickiness" {
-    for_each = var.stickness
+    for_each = try(each.value.stickness, [])
     content {
       enabled         = try(stickiness.value.enabled, false)
       type            = try(stickiness.value.type, "lb_cookie")
@@ -37,7 +33,7 @@ resource "aws_lb_target_group" "this" {
     }
   }
   dynamic "health_check" {
-    for_each = var.health_check
+    for_each = try(each.value.health_check, [])
     content {
       enabled             = try(health_check.value.enabled, false)
       healthy_threshold   = try(health_check.value.healthy_threshold, 3)
@@ -51,14 +47,14 @@ resource "aws_lb_target_group" "this" {
     }
   }
   dynamic "target_failover" {
-    for_each = var.target_failover
+    for_each = try(each.value.target_failover, [])
     content {
       on_deregistration = try(target_failover.value.on_deregistration, "no_rebalance")
       on_unhealthy      = try(target_failover.value.on_failure, "no_rebalance")
     }
   }
   dynamic "target_health_state" {
-    for_each = var.target_health_state
+    for_each = try(each.value.target_health_state, [])
     content {
       enable_unhealthy_connection_termination = try(target_health_state.value.enable_unhealthy_connection_termination, true)
     }
