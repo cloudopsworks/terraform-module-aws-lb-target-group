c9d524181435718d5a10dbfb35ca3456a1563ae3
Cristian Beraha <cristian@cloudops.works>
2024-07-06 00:31:52 -0300

    chore: listener rules settings

--- main.tf
+++ main.tf
@@ -62,7 +62,7 @@ resource "aws_lb_target_group" "this" {
   tags = local.all_tags
 }
 
-
+# Target group attachment - with target IDs
 resource "aws_lb_target_group_attachment" "this" {
   for_each = merge([
     for k, v in var.target_groups : {
@@ -74,4 +74,109 @@ resource "aws_lb_target_group_attachment" "this" {
   ]...)
   target_group_arn = each.value.target_group_arn
   target_id        = each.value.target_id
+}
+
+# Listener rule specific for ALB
+resource "aws_lb_listener_rule" "lb_rule" {
+  for_each     = var.listener_rules
+  listener_arn = each.value.listener_arn
+  priority     = try(each.value.priority, 100)
+  action {
+    type             = each.value.actions.type
+    target_group_arn = each.value.tg_ref != "" ? aws_lb_target_group.this[each.value.tg_ref].arn : null
+    order            = each.value.actions.order
+    dynamic "authenticate_cognito" {
+      for_each = try(each.value.actions.authenticate_cognito, [])
+      content {
+        authentication_request_extra_params = authenticate_cognito.value.authentication_request_extra_params
+        on_unauthenticated_request          = authenticate_cognito.value.on_unauthenticated_request
+        scope                               = authenticate_cognito.value.scope
+        session_cookie_name                 = authenticate_cognito.value.session_cookie_name
+        session_timeout                     = authenticate_cognito.value.session_timeout
+        user_pool_arn                       = authenticate_cognito.value.user_pool_arn
+        user_pool_client_id                 = authenticate_cognito.value.user_pool_client_id
+        user_pool_domain                    = authenticate_cognito.value.user_pool_domain
+      }
+
+    }
+    dynamic "authenticate_oidc" {
+      for_each = try(each.value.actions.authenticate_oidc, [])
+      content {
+        authentication_request_extra_params = authenticate_oidc.value.authentication_request_extra_params
+        authorization_endpoint              = authenticate_oidc.value.authorization_endpoint
+        client_id                           = authenticate_oidc.value.client_id
+        client_secret                       = authenticate_oidc.value.client_secret
+        issuer                              = authenticate_oidc.value.issuer
+        on_unauthenticated_request          = authenticate_oidc.value.on_unauthenticated_request
+        scope                               = authenticate_oidc.value.scope
+        session_cookie_name                 = authenticate_oidc.value.session_cookie_name
+        session_timeout                     = authenticate_oidc.value.session_timeout
+        token_endpoint                      = authenticate_oidc.value.token_endpoint
+        user_info_endpoint                  = authenticate_oidc.value.user_info_endpoint
+      }
+    }
+    dynamic "fixed_response" {
+      for_each = try(each.value.actions.fixed_response, [])
+      content {
+        content_type = fixed_response.value.content_type
+        message_body = fixed_response.value.message_body
+        status_code  = fixed_response.value.status_code
+      }
+    }
+    dynamic "forward" {
+      for_each = try(each.value.actions.forward, [])
+      content {
+        stickiness       = try(forward.value.stickiness, null)
+        target_group_arn = forward.value.target_group_arn
+      }
+    }
+    dynamic "redirect" {
+      for_each = try(each.value.actions.redirect, [])
+      content {
+        host        = try(redirect.value.host, "#{host}")
+        path        = try(redirect.value.path, "#{path}")
+        port        = try(redirect.value.port, "#{port}")
+        protocol    = try(redirect.value.protocol, "#{protocol}")
+        query       = try(redirect.value.query, "#{query}")
+        status_code = try(redirect.value.status_code, "HTTP_302")
+      }
+
+    }
+  }
+  condition {
+    dynamic "host_header" {
+      for_each = try(each.value.conditions.host_header, [])
+      content {
+        values = host_header.value.values
+      }
+    }
+    dynamic "http_header" {
+      for_each = try(each.value.conditions.http_header, [])
+      content {
+        http_header_name = http_header.value.http_header_name
+        values           = http_header.value.values
+      }
+    }
+    dynamic "path_pattern" {
+      for_each = try(each.value.conditions.path_pattern, [])
+      content {
+        values = path_pattern.value.values
+      }
+    }
+    dynamic "query_string" {
+      for_each = try(each.value.conditions.query_string, [])
+      content {
+        key    = query_string.value.key
+        values = query_string.value.values
+      }
+    }
+    dynamic "source_ip" {
+      for_each = try(each.value.conditions.source_ip, [])
+      content {
+        values = source_ip.value.values
+      }
+    }
+  }
+
+  tags = local.all_tags
 }
