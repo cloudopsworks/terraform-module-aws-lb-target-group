5f152f9f37c139b4352efbcdb8bb247af9bc1788
Cristian Beraha <cristian@cloudops.works>
2024-07-06 20:57:01 -0300

    fix: target group attachment change

--- output.tf
+++ output.tf
@@ -16,3 +16,27 @@ output "target_groups" {
     }
   }
 }
+
+output "target_group_attachments" {
+  value = {
+    for attachment in aws_lb_target_group_attachment.this : attachment.id => {
+      id                = attachment.id
+      target_group_arn  = attachment.target_group_arn
+      target_id         = attachment.target_id
+      availability_zone = attachment.availability_zone
+      port              = attachment.port
+    }
+  }
+}
+
+output "listener_rules" {
+  value = {
+    for rule in aws_lb_listener_rule.lb_rule : rule.id => {
+      id           = rule.id
+      listener_arn = rule.listener_arn
+      priority     = rule.priority
+      actions      = rule.action
+      conditions   = rule.condition
+    }
+  }
+}
